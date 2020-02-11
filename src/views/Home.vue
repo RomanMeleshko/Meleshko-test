3bd3e94aaad8917e031f7496d878cbcc96bd21da
RomanMeleshko <roman.meleshko1@gmail.com>
2020-02-11 20:14:19 +0200

    create app

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  methods: {
+    axios.get("http://openweathermap.org/.")
+    .then((response) => {
+
+     }).catch(error => {
+         console.log("error");
+            })
+  },
+  components: {
+
+  }
+}
+</script>
